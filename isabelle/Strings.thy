4500d004e01948607f7bfe88d68d668b866d602a
Ioannis Filippidis <jfilippidis@gmail.com>
2020-12-26 17:15:59 +0200

    MAI: update `--` comments in theory `Strings` to Isabelle2020

--- isabelle/Strings.thy
+++ isabelle/Strings.thy
@@ -22,7 +22,7 @@ text \<open>
 definition Nibble
 where "Nibble \<equiv> {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}"
 
-definition char   -- {* @{text char} is intended to be applied to nibbles *}
+definition char   (* -- @{text char} is intended to be applied to nibbles *)
 where "char(a,b) \<equiv> \<langle>a,b\<rangle>"
 
 lemma charInj [simp]: "(char(a,b) = char(c,d)) = (a=c \<and> b=d)"
