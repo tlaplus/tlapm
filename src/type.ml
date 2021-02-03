8004e8d053e9dfb3b12f5f89f82f511f0c3e26a9
adef <antoine.defourne@inria.fr>
2021-02-03 17:23:15 +0100

    Typechecking visitor

--- src/type.ml
+++ src/type.ml
@@ -5,6 +5,7 @@
 Revision.f "$Rev$"
 
 module T = T_t
+module Visit = T_visit
 module Collect = T_collect
 module Reconstruct = T_recon
 
--- src/type.mli
+++ src/type.mli
@@ -76,6 +76,37 @@ module T : sig
   val pp_print_atm : Format.formatter -> atm -> unit
 end
 
+module Visit : sig
+  open Expr.T
+  open T
+
+  type 's scx = 's * hyp Deque.dq
+
+  val adj  : 's scx -> hyp -> 's scx
+  val adjs : 's scx -> hyp list -> 's scx
+
+  val lookup_ty0 : 's scx -> int -> ty0
+  val lookup_ty1 : 's scx -> int -> ty1
+  val lookup_ty2 : 's scx -> int -> ty2
+
+  class virtual ['s, 'a] foldmap : object
+    method expr     : 's scx -> 'a -> expr -> 'a * expr * ty0
+    method earg     : 's scx -> 'a -> expr -> 'a * expr * ty1
+    method eopr     : 's scx -> 'a -> expr -> 'a * expr * ty2
+    method pform    : 's scx -> 'a -> pform -> 'a * pform
+    method sel      : 's scx -> 'a -> sel -> 'a * sel
+    method sequent  : 's scx -> 'a -> sequent -> 's scx * 'a * sequent
+    method defn     : 's scx -> 'a -> defn -> 'a * defn
+    method defns    : 's scx -> 'a -> defn list -> 's scx * 'a * defn list
+    method bounds   : 's scx -> 'a -> bound list -> 's scx * 'a * bound list
+    method bound    : 's scx -> 'a -> bound -> 's scx * 'a * bound
+    method exspec   : 's scx -> 'a -> exspec -> 'a * exspec * ty0 list
+    method instance : 's scx -> 'a -> instance -> 'a * instance
+    method hyp      : 's scx -> 'a -> hyp -> 's scx * 'a * hyp
+    method hyps     : 's scx -> 'a -> hyp Deque.dq -> 's scx * 'a * hyp Deque.dq
+  end
+end
+
 module Collect : sig
   open Expr.T
   open T
