8b133722c7242cbd472b90a27fa25746e0a37038
adef <antoine.defourne@inria.fr>
2022-11-15 17:21:51 +0100

    New translation to SMT-LIB
    The old version is still here, use it with the flag
        --debug oldsmt

--- src/backend/prep.ml
+++ src/backend/prep.ml
@@ -442,38 +442,48 @@ let gen_smt_solve suffix exec desc fmt_expr meth ob org_ob f res_cont comm =
     Schedule.Immediate (res_cont w (Method.NotTried msg) None)
 
 
+(* FIXME Remove all the oldsmt code *)
+let get_encode_smtlib () =
+  if Params.debugging "oldsmt" then Smt.encode_smtlib
+  else Smt.encode_smtlib
+
+(* FIXME *)
+let get_encode_fof () =
+  if Params.debugging "oldsmt" then Smt.encode_fof
+  else Smtlib.pp_print_obligation ~solver:"fof"
+
 let smt_solve ob org_ob f res_cont =
-  gen_smt_solve ".smt" Params.smt "default SMT solver" Smt.encode_smtlib
+  gen_smt_solve ".smt" Params.smt "default SMT solver" (get_encode_smtlib ())
                 (Method.Smt3 f) ob org_ob f res_cont ";;"
 
 
 let cvc3_solve ob org_ob f res_cont =
-  gen_smt_solve ".smt" Params.cvc4 "CVC4" Smt.encode_smtlib
+  gen_smt_solve ".smt" Params.cvc4 "CVC4" (get_encode_smtlib () ~solver:"CVC4")
                 (Method.Cvc33 f) ob org_ob f res_cont ";;"
 
 
 let yices_solve ob org_ob f res_cont =
-  gen_smt_solve ".ys" Params.yices "Yices" Smt.encode_smtlib
+  gen_smt_solve ".ys" Params.yices "Yices" (get_encode_smtlib ())
                 (Method.Yices3 f) ob org_ob f res_cont ";;"
 
 
 let z3_solve ob org_ob f res_cont =
-  gen_smt_solve ".smt2" Params.z3 "Z3" (Smt.encode_smtlib ~solver:"Z3")
+  gen_smt_solve ".smt2" Params.z3 "Z3" ((get_encode_smtlib ()) ~solver:"Z3")
                 (Method.Z33 f) ob org_ob f res_cont ";;"
 
 
 let verit_solve ob org_ob f res_cont =
-  gen_smt_solve ".smt2" Params.verit "veriT" Smt.encode_smtlib
+  gen_smt_solve ".smt2" Params.verit "veriT" (get_encode_smtlib () ~solver:"veriT")
                 (Method.Verit f) ob org_ob f res_cont ";;"
 
 
 let spass_solve ob org_ob f res_cont =
-  gen_smt_solve ".tptp" Params.spass_tptp "Spass" Smt.encode_fof
+  gen_smt_solve ".tptp" Params.spass_tptp "Spass" (get_encode_fof ())
                 (Method.Spass f) ob org_ob f res_cont "%%"
 
 
 let tptp_solve ob org_ob f res_cont =
-  gen_smt_solve ".tptp" Params.eprover "Tptp" Smt.encode_fof
+  gen_smt_solve ".tptp" Params.eprover "Tptp" (get_encode_fof ())
                 (Method.Tptp f) ob org_ob f res_cont "% "
 
 
---
+++ src/backend/smtlib.mli
@@ -0,0 +1,12 @@
+(*
+ * backend/smtlib.ml --- direct translation to SMT-LIB
+ *
+ *
+ * Copyright (C) 2008-2010  INRIA and Microsoft Corporation
+ *)
+
+(** Print in SMT-LIB format a sequent reduced to first-order logic
+    without TLA+ primitives
+*)
+val pp_print_obligation : ?solver:string -> Format.formatter -> Proof.T.obligation -> unit;;
+
