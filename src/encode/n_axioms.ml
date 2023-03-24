194f684dc06e8d60ebe9ded80edc026ec455542c
adef-inr <antoine.defourne@laposte.net>
2023-03-24 14:06:49 +0100

    Update src/encode/n_axioms.ml
    Co-authored-by: Damien Doligez <damien.doligez@inria.fr>

--- src/encode/n_axioms.ml
+++ src/encode/n_axioms.ml
@@ -2,7 +2,7 @@
  * encode/axioms.ml --- axioms for TLA+ symbols
  *
  *
- * Copyright (C) 2008-2010  INRIA and Microsoft Corporation
+ * Copyright (C) 2022  INRIA and Microsoft Corporation
  *)
 
 open Ext
