0e323ce407bee06900d90fc887681d5b1467ac2a
adef-inr <antoine.defourne@laposte.net>
2023-03-24 14:06:04 +0100

    Update src/type/t_visit.mli
    Co-authored-by: Damien Doligez <damien.doligez@inria.fr>

--- src/type/t_visit.mli
+++ src/type/t_visit.mli
@@ -2,7 +2,7 @@
  * type/visit.mli --- visitors with syntactic typechecking
  *
  *
- * Copyright (C) 2008-2010  INRIA and Microsoft Corporation
+ * Copyright (C) 2022  INRIA and Microsoft Corporation
  *)
 
 open Expr.T
