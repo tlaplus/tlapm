7590d73dac77175db1fca39e0c8aa8fdc4291079
adef-inr <antoine.defourne@laposte.net>
2023-03-24 14:15:10 +0100

    Update src/type/t_synth.ml
    Co-authored-by: Damien Doligez <damien.doligez@inria.fr>

--- src/type/t_synth.ml
+++ src/type/t_synth.ml
@@ -2,7 +2,7 @@
  * type/synth.ml --- decorate TLA+ expressions with types
  *
  *
- * Copyright (C) 2008-2010  INRIA and Microsoft Corporation
+ * Copyright (C) 2022  INRIA and Microsoft Corporation
  *)
 
 open Expr.T
