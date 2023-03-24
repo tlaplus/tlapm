1bf59471e152266521b8b0a32417ed1a3b02eca6
adef-inr <antoine.defourne@laposte.net>
2023-03-24 14:17:02 +0100

    Update src/backend/smtlib.mli
    Co-authored-by: Damien Doligez <damien.doligez@inria.fr>

--- src/backend/smtlib.mli
+++ src/backend/smtlib.mli
@@ -2,7 +2,7 @@
  * backend/smtlib.ml --- direct translation to SMT-LIB
  *
  *
- * Copyright (C) 2008-2010  INRIA and Microsoft Corporation
+ * Copyright (C) 2022  INRIA and Microsoft Corporation
  *)
 
 (** Print in SMT-LIB format a sequent reduced to first-order logic
