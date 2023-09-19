e71f9364a83204db079547f47f4bcf9c63b9a799
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-09-20 01:02:18 +0300

    Improving it.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/tlapm_lsp_lib.ml
+++ lsp/lib/tlapm_lsp_lib.ml
@@ -1,23 +1,19 @@
 (** Here we construct a decoder/encoder for the LSP protocol on top of Eio flows. *)
 module EioLspCodec : sig
-  type 'a io_res = IoOk of 'a | IoErr of exn
-
-  val read : Eio.Buf_read.t -> Jsonrpc.Packet.t option io_res
-  val write : Eio.Buf_write.t -> Jsonrpc.Packet.t -> unit io_res
+  val read : Eio.Buf_read.t -> (Jsonrpc.Packet.t option, exn) result
+  val write : Eio.Buf_write.t -> Jsonrpc.Packet.t -> (unit, exn) result
 end = struct
-  type 'a io_res = IoOk of 'a | IoErr of exn
-
   module IoState = struct
-    type 'a t = 'a io_res
+    type 'a t = ('a, exn) result
 
-    let return x = IoOk x
-    let raise exn = IoErr exn
+    let return x = Ok x
+    let raise exn = Error exn
 
     module O = struct
       let ( let+ ) st f =
-        match st with IoOk a -> IoOk (f a) | IoErr exn -> IoErr exn
+        match st with Ok a -> Ok (f a) | Error exn -> Error exn
 
-      let ( let* ) st f = match st with IoOk a -> f a | IoErr exn -> IoErr exn
+      let ( let* ) st f = match st with Ok a -> f a | Error exn -> Error exn
     end
   end
 
@@ -27,15 +23,15 @@ end = struct
 
     let read_line (buf : input) : string option IoState.t =
       let line = Eio.Buf_read.line buf in
-      IoOk (Some line)
+      Ok (Some line)
 
     let read_exactly (buf : input) (n : int) : string option IoState.t =
       let data = Eio.Buf_read.take n buf in
-      IoOk (Some data)
+      Ok (Some data)
 
     let rec write (buf : output) (lines : string list) : unit IoState.t =
       match lines with
-      | [] -> IoOk ()
+      | [] -> Ok ()
       | line :: tail ->
           let () = Eio.Buf_write.string buf line in
           write buf tail
@@ -54,22 +50,40 @@ end = struct
   (* // TODO: curl -vvv -X POST -H 'Content-Type: application/vscode-jsonrpc; charset=utf-8' -d '{"jsonrpc": "2.0", "id": 1, "method": "textDocument/completion", "params": { }}' http://localhost:8080/ *)
   let lsp_packet_handler (packet : Jsonrpc.Packet.t) write_fun =
     Eio.traceln "Got LSP Packet";
-    write_fun packet (* // TODO: That's just echo. *)
+    let jsonrpc_req_handler jsonrpc_req =
+      match Lsp.Client_request.of_jsonrpc jsonrpc_req with
+      | Ok (E (Initialize _params)) -> () (* TODO: Handle all the cases. *)
+      | _ -> ()
+    in
+    let jsonrpc_notif_handler jsonrpc_notif =
+      match Lsp.Client_notification.of_jsonrpc jsonrpc_notif with (* TODO: Handle all the cases. *)
+      | Ok Initialized -> ()
+      | Ok (TextDocumentDidOpen _params) -> ()
+      | Ok (UnknownNotification _params) ->
+          write_fun packet (* // TODO: That's just echo. *)
+      | _ -> ()
+    in
+    match packet with
+    | Notification x -> jsonrpc_notif_handler x
+    | Request jsonrpc_req -> jsonrpc_req_handler jsonrpc_req
+    | Response _ -> () (* // TODO: Handle it. *)
+    | Batch_call _ -> () (* // TODO: Handle it. *)
+    | Batch_response _ -> ()
 
   let rec lsp_stream_handler buf_r buf_w =
     let open EioLspCodec in
     let write_fun out_packet =
       match write buf_w out_packet with
-      | IoOk () -> Eio.Buf_write.flush buf_w
-      | IoErr exn ->
+      | Ok () -> Eio.Buf_write.flush buf_w
+      | Error exn ->
           Eio.traceln "IO Error reading packet: %s" (Printexc.to_string exn)
     in
     match read buf_r with
-    | IoOk (Some packet) ->
+    | Ok (Some packet) ->
         lsp_packet_handler packet write_fun;
         lsp_stream_handler buf_r buf_w
-    | IoOk None -> Eio.traceln "No packet was read."
-    | IoErr exn ->
+    | Ok None -> Eio.traceln "No packet was read."
+    | Error exn ->
         Eio.traceln "IO Error reading packet: %s" (Printexc.to_string exn)
 
   let err_handler = Eio.traceln "Error handling connection: %a" Fmt.exn
