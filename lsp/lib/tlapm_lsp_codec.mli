7b6735fb16a1b3e019be2f2b467a967b92b12aca
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-09-20 23:56:29 +0300

    IO Tracing added.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/tlapm_lsp_codec.mli
+++ lsp/lib/tlapm_lsp_codec.mli
@@ -3,5 +3,9 @@ Here we construct a decoder/encoder for the LSP protocol on top of Eio flows.
 We use the lsp module from the ocaml-lsp server and configure it to run over Eio.
 *)
 
-val read : Eio.Buf_read.t -> (Jsonrpc.Packet.t option, exn) result
-val write : Eio.Buf_write.t -> Jsonrpc.Packet.t -> (unit, exn) result
+type trace_fun = string -> unit
+type input_chan = Eio.Buf_read.t * trace_fun
+type output_chan = Eio.Buf_write.t * trace_fun
+
+val read : input_chan -> (Jsonrpc.Packet.t option, exn) result
+val write : output_chan -> Jsonrpc.Packet.t -> (unit, exn) result
--- lsp/lib/tlapm_lsp_server.mli
+++ lsp/lib/tlapm_lsp_server.mli
@@ -6,4 +6,5 @@ This module contains only the generic server-related functions,
 the tlapm-specifics is moved to tlapm_lsp_packets.
 *)
 
-val run : transport -> Eio_unix.Stdenv.base -> string Eio.Std.Promise.t -> unit
+val run :
+  transport -> bool -> Eio_unix.Stdenv.base -> string Eio.Std.Promise.t -> unit
