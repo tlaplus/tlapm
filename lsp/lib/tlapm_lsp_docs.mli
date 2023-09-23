8504e0c7d8496c903ab3ad22efeebf92c0a67db3
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-09-23 23:48:38 +0300

    Docs store.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

---
+++ lsp/lib/tlapm_lsp_docs.mli
@@ -0,0 +1,19 @@
+(** Here we maintain a list of documents and their revisions. *)
+
+type t
+(** A document store type. *)
+
+type tk = Lsp.Types.DocumentUri.t
+(** Key type to identify documents. *)
+
+val empty : t
+(** Create new empty document store. *)
+
+val add : t -> tk -> int -> string -> t
+(** Either add document or its revision. Forgets all previous unused revisions. *)
+
+val rem : t -> tk -> t
+(** Remove a document with all its revisions. *)
+
+val get_opt : t -> tk -> (string * int) option
+(** Lookup document's last revision. *)
