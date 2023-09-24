31d69e11753f1b93de74009fa7711bb7d92c909f
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-09-24 21:05:02 +0300

    Playing with more LSP command types.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/tlapm_lsp_packets.ml
+++ lsp/lib/tlapm_lsp_packets.ml
@@ -14,7 +14,7 @@ let reply_error (jsonrpc_req : Jsonrpc.Request.t) writer code message =
   writer packet
 
 (** Dispatch notification packets. *)
-let handle_jsonrpc_notif jsonrpc_notif _writer state =
+let handle_jsonrpc_notif jsonrpc_notif writer state =
   let open Lsp.Types in
   match Lsp.Client_notification.of_jsonrpc jsonrpc_notif with
   | Ok Initialized ->
@@ -25,8 +25,30 @@ let handle_jsonrpc_notif jsonrpc_notif _writer state =
       let vsn = params.textDocument.version in
       let text = params.textDocument.text in
       Eio.traceln "DOCUMENT[Open]: %s => %s" (DocumentUri.to_string uri) text;
-      Tlapm_lsp_state.handle_if_ready_silent state (fun docs ->
-          Tlapm_lsp_docs.add docs uri vsn text)
+      let send_diags () =
+        let some =
+          Diagnostic.create ~message:"Hey from prover!"
+            ~range:
+              (Range.create
+                 ~start:(Position.create ~line:1 ~character:3)
+                 ~end_:(Position.create ~line:1 ~character:7))
+            ()
+        in
+        let dpar =
+          PublishDiagnosticsParams.create ~diagnostics:[ some ] ~uri
+            ~version:vsn ()
+        in
+        let diag = Lsp.Server_notification.PublishDiagnostics dpar in
+        let dpkg =
+          Jsonrpc.Packet.Notification (Lsp.Server_notification.to_jsonrpc diag)
+        in
+        writer dpkg
+      in
+      let with_docs docs =
+        send_diags ();
+        Tlapm_lsp_docs.add docs uri vsn text
+      in
+      Tlapm_lsp_state.handle_if_ready_silent state with_docs
   | Ok (TextDocumentDidChange params) -> (
       let uri = params.textDocument.uri in
       let vsn = params.textDocument.version in
@@ -78,14 +100,26 @@ let handle_jsonrpc_req_initialize (jsonrpc_req : Jsonrpc.Request.t) params
         Eio.traceln "CONN: Client INFO, name=%s, version=%s" ci.name ci_version
   in
   print_ci params;
-  let supported_commands = [ "tlapm-lsp-test.prover-info" ] in
+  let supported_commands =
+    [ "tlapm-lsp-test.prover-info"; "tlapm-lsp-test.prove-step" ]
+  in
   let capabilities =
     ServerCapabilities.create
       ~textDocumentSync:(`TextDocumentSyncKind TextDocumentSyncKind.Full)
       ~executeCommandProvider:
-        (* TODO: workDoneProgress:false *)
         (ExecuteCommandOptions.create ~commands:supported_commands
            ~workDoneProgress:false ())
+      ~diagnosticProvider:
+        (`DiagnosticOptions
+          (DiagnosticOptions.create ~identifier:"TLAPM"
+             ~interFileDependencies:false ~workspaceDiagnostics:false
+             ~workDoneProgress:false ()))
+      ~codeActionProvider:
+        (`CodeActionOptions
+          (CodeActionOptions.create ~resolveProvider:true
+             ~workDoneProgress:false
+             ~codeActionKinds:[ CodeActionKind.Other "proof-state" ]
+             ()))
       ()
   in
   let server_version =
@@ -107,7 +141,8 @@ let handle_jsonrpc_req_shutdown (_jsonrpc_req : Jsonrpc.Request.t) state =
 
 let handle_jsonrpc_req_unknown (jsonrpc_req : Jsonrpc.Request.t) message writer
     state =
-  Eio.traceln "Received unknown JsonRPC request, method=%s" jsonrpc_req.method_;
+  Eio.traceln "Received unknown JsonRPC request, method=%s, error=%s"
+    jsonrpc_req.method_ message;
   let open Jsonrpc.Response.Error in
   reply_error jsonrpc_req writer Code.MethodNotFound message;
   state
@@ -120,10 +155,50 @@ let handle_jsonrpc_req_exec_cmd (jsonrpc_req : Jsonrpc.Request.t)
       Eio.traceln "COMMAND: prover-info";
       reply_ok jsonrpc_req writer (`String "OK");
       state
+  | "tlapm-lsp-test.prove-step" ->
+      (* TODO: Args are passed from the codeAction. *)
+      Eio.traceln "COMMAND: prove-step";
+      reply_ok jsonrpc_req writer (`String "OK");
+      state
   | unknown ->
       handle_jsonrpc_req_unknown jsonrpc_req
         (Printf.sprintf "command unknown: %s" unknown)
         writer state
+
+(**
+Provide code actions for a document.
+  - Code actions can be used for proof decomposition, probably.
+*)
+let handle_jsonrpc_req_code_action (jsonrpc_req : Jsonrpc.Request.t)
+    (_params : Lsp.Types.CodeActionParams.t) writer state =
+  let open Lsp.Types in
+  let someActionDiag =
+    Diagnostic.create ~message:"Hey from prover as an action!"
+      ~range:
+        (Range.create
+           ~start:(Position.create ~line:2 ~character:13)
+           ~end_:(Position.create ~line:2 ~character:17))
+      ()
+  in
+  let someAction =
+    Lsp.Types.CodeAction.create ~title:"Prover code action"
+      ~diagnostics:[ someActionDiag ]
+      ~command:
+        (Command.create ~command:"tlapm-lsp-test.prove-step"
+           ~title:"Prove it as an action!"
+           ~arguments:[ `String "important_arg" ]
+           ())
+      ()
+  in
+  let acts = Some [ `CodeAction someAction ] in
+  reply_ok jsonrpc_req writer (Lsp.Types.CodeActionResult.yojson_of_t acts);
+  state
+
+let handle_jsonrpc_req_code_action_resolve (jsonrpc_req : Jsonrpc.Request.t)
+    (_params : Lsp.Types.CodeAction.t) writer state =
+  (* TODO: Actually resolve the code actions. *)
+  reply_ok jsonrpc_req writer (`String "OK");
+  state
 
 (** Dispatch request packets. *)
 let handle_jsonrpc_request (jsonrpc_req : Jsonrpc.Request.t) writer state =
@@ -133,9 +208,24 @@ let handle_jsonrpc_request (jsonrpc_req : Jsonrpc.Request.t) writer state =
       handle_jsonrpc_req_initialize jsonrpc_req params writer state
   | Ok (E (ExecuteCommand params)) ->
       handle_jsonrpc_req_exec_cmd jsonrpc_req params writer state
+  | Ok (E (CodeAction params)) ->
+      handle_jsonrpc_req_code_action jsonrpc_req params writer state
+  | Ok (E (CodeActionResolve params)) ->
+      handle_jsonrpc_req_code_action_resolve jsonrpc_req params writer state
   | Ok (E Shutdown) -> handle_jsonrpc_req_shutdown jsonrpc_req state
-  | _ ->
+  | Ok (E (UnknownRequest unknown)) -> (
+      match unknown.meth with
+      | "textDocument/diagnostic" ->
+          (* TODO: Handle the diagnostic pull. *)
+          state
+      | _ ->
+          handle_jsonrpc_req_unknown jsonrpc_req "unknown method not supported"
+            writer state)
+  | Ok (E _unsupported) ->
       handle_jsonrpc_req_unknown jsonrpc_req "method not supported" writer state
+  | Error reason ->
+      let err_msg = Printf.sprintf "cannot decode method: %s" reason in
+      handle_jsonrpc_req_unknown jsonrpc_req err_msg writer state
 
 (* Dispatch client responses to our requests. *)
 let handle_jsonrpc_response _jsonrpc_resp state = state
