85062bb2ab2748af537c7751808f7426cd185523
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-10-02 09:50:14 +0300

    Some progress on the tlapm invokation.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/tlapm_lsp_prover.mli
+++ lsp/lib/tlapm_lsp_prover.mli
@@ -1,9 +1,40 @@
 type t
 
+module ToolboxProtocol : sig
+  type tlapm_obl_state =
+    | ToBeProved
+    | BeingProved
+    | Normalized
+    | Proved
+    | Failed
+    | Interrupted
+    | Trivial
+    | Unknown of string
+
+  type tlapm_loc = (int * int) * (int * int)
+
+  type tlapm_msg =
+    | TlapmWarning of { msg : string }
+    | TlapmError of { url : string; msg : string }
+    | TlapmObligationsNumber of int
+    | TlapmObligation of {
+        id : int;
+        loc : tlapm_loc;
+        status : tlapm_obl_state;
+        fp : string option;
+        prover : string option;
+        meth : string option;
+        reason : string option;
+        already : bool option;
+        obl : string option;
+      }
+end
+
 val create :
   Eio.Switch.t ->
   Eio__.Fs.dir_ty Eio.Path.t ->
   Eio_unix.Process.mgr_ty Eio.Process.mgr ->
+  ToolboxProtocol.tlapm_msg Eio.Stream.t ->
   Tlapm_lsp_docs.t ->
   t
 
