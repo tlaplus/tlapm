dede4da3e0a08a4c4f318677fd1f0cc3df263175
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-10-07 17:28:18 +0300

    Code reorganized to run on threads.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/tlapm_lsp_packets.mli
+++ lsp/lib/tlapm_lsp_packets.mli
@@ -1,7 +1,16 @@
 (** Here we have all the TLAPM specific LSP action handlers. *)
 
-val handle_jsonrpc_packet :
-  Jsonrpc.Packet.t ->
-  (Jsonrpc.Packet.t -> unit) ->
-  Tlapm_lsp_state.t ->
-  Tlapm_lsp_state.t
+module Docs = Tlapm_lsp_docs
+
+module type Callbacks = sig
+  type cb_t
+
+  val ready : cb_t -> cb_t
+  val shutdown : cb_t -> cb_t
+  val with_docs : cb_t -> (Docs.t -> Docs.t) -> cb_t
+end
+
+module Make (CB : Callbacks) : sig
+  val handle_jsonrpc_packet :
+    Jsonrpc.Packet.t -> (Jsonrpc.Packet.t -> unit) -> CB.cb_t -> CB.cb_t
+end
---
+++ lsp/lib/tlapm_lsp_session.ml
@@ -0,0 +1,107 @@
+module Docs = Tlapm_lsp_docs
+
+type events =
+  | LspEOF
+  | LspPacket of Jsonrpc.Packet.t
+  | TlapmEvent of int * Tlapm_lsp_prover.ToolboxProtocol.tlapm_msg
+
+type mode = Initializing | Ready | Shutdown
+
+type t = {
+  events : unit -> events;
+  output : Jsonrpc.Packet.t option -> unit;
+  mode : mode;
+  docs : Docs.t;
+}
+
+module PacketsCB = struct
+  type cb_t = t
+
+  let ready st =
+    match st.mode with
+    | Initializing -> { st with mode = Ready }
+    | Ready -> st
+    | Shutdown -> st
+
+  let shutdown st =
+    match st.mode with
+    | Initializing -> { st with mode = Shutdown }
+    | Ready -> { st with mode = Shutdown }
+    | Shutdown -> st
+
+  let with_docs' st f =
+    match st.mode with
+    | Initializing -> Error "initializing"
+    | Ready -> Ok { st with docs = f st.docs }
+    | Shutdown -> Error "going to shutdown"
+
+  let with_docs st f =
+    match with_docs' st f with
+    | Ok st' -> st'
+    | Error err ->
+        Eio.traceln "Ignoring request: %s" err;
+        st
+
+  let%test_unit "basics" =
+    let st =
+      {
+        mode = Initializing;
+        output = (fun _ -> ());
+        events = (fun () -> LspEOF);
+        docs = Docs.empty;
+      }
+    in
+    let () =
+      match with_docs' st (fun docs -> docs) with
+      | Ok _ -> failwith "should fail"
+      | Error _ -> ()
+    in
+    let st = ready st in
+    let st =
+      match with_docs' st (fun docs -> docs) with
+      | Ok st -> st
+      | Error e -> failwith e
+    in
+    let st = shutdown st in
+    let () =
+      match with_docs' st (fun docs -> docs) with
+      | Ok _ -> failwith "should fail"
+      | Error _ -> ()
+    in
+    ()
+end
+
+module Packets = Tlapm_lsp_packets.Make (PacketsCB)
+
+let handle_lsp_packet p st =
+  let writer o = st.output (Some o) in
+  Some (Packets.handle_jsonrpc_packet p writer st)
+
+let handle_tlapm_msg _ref _msg st =
+  (* TODO: Implement. *)
+  Some st
+
+let handle_event e st =
+  match e with
+  | LspEOF -> None
+  | LspPacket p -> handle_lsp_packet p st
+  | TlapmEvent (ref, msg) -> handle_tlapm_msg ref msg st
+
+(* The main event processing loop.
+   At the exit we send EOF to the output thread. *)
+let rec loop st =
+  match handle_event (st.events ()) st with
+  | Some st' -> loop st'
+  | None -> st.output None
+
+(** Entry point to run the session as a fiber. *)
+let run event_taker output_adder =
+  let st =
+    {
+      mode = Initializing;
+      events = event_taker;
+      output = output_adder;
+      docs = Docs.empty;
+    }
+  in
+  loop st
