7870b0a4f4c777fc19704bc1029e38ef28ca8f7b
Karolis Petrauskas <k.petrauskas@gmail.com>
2023-10-14 09:48:10 +0300

    Use modules instead of embedded recs.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/tlapm_lsp_docs.ml
+++ lsp/lib/tlapm_lsp_docs.ml
@@ -1,4 +1,4 @@
-(* cSpell:words printexc *)
+(* cSpell:words printexc recursives submod anoninst *)
 
 (* Max number of unprocessed/pending versions to keep. *)
 let keep_vsn_count = 50
@@ -16,34 +16,45 @@ module OblMap = Map.Make (OblRef)
 open Tlapm_lsp_prover
 open Tlapm_lsp_prover.ToolboxProtocol
 
-(* Versions that are collected after the last prover launch or client
-   asks for diagnostics. We store some limited number of versions here,
-   just to cope with async events from the client. *)
-type tv =
-  | TV of {
-      text : string; (* Contents if the file at the specific version. *)
-      version : int;
-    }
+(** Versions that are collected after the last prover launch or client
+    asks for diagnostics. We store some limited number of versions here,
+    just to cope with async events from the client. *)
+module TV = struct
+  type t = {
+    text : string; (* Contents if the file at the specific version. *)
+    version : int;
+  }
 
-(* Actual state of the document. *)
-type ta =
-  | TA of {
-      doc_vsn : tv;
-      p_ref : int;
-      mule : (Tlapm_lib.Module.T.mule, exn) result option;
-      nts : tlapm_notif list;
-      obs : tlapm_obligation OblMap.t;
-    }
+  let make txt vsn = { text = txt; version = vsn }
+end
 
-type td =
-  | TD of {
-      pending : tv list; (* All the received but not yet processed versions. *)
-      actual : ta option; (* Already processed version. *)
-      last_p_ref : int; (* Counter for the proof runs. *)
-    }
+(** Actual state of the document. *)
+module TA = struct
+  type t = {
+    doc_vsn : TV.t;
+    p_ref : int;
+    mule : (Tlapm_lib.Module.T.mule, exn) result option;
+    nts : tlapm_notif list;
+    obs : tlapm_obligation OblMap.t;
+  }
+
+  let make tv =
+    { doc_vsn = tv; p_ref = 0; mule = None; obs = OblMap.empty; nts = [] }
+end
+
+(** Represents a document identified by its uri. It can contain multiple versions and all the related info. *)
+module TD = struct
+  type t = {
+    pending : TV.t list; (* All the received but not yet processed versions. *)
+    actual : TA.t option; (* Already processed version. *)
+    last_p_ref : int; (* Counter for the proof runs. *)
+  }
+
+  let make tv = { pending = [ tv ]; actual = None; last_p_ref = 0 }
+end
 
 type tk = Lsp.Types.DocumentUri.t
-type t = td DocMap.t
+type t = TD.t DocMap.t
 type proof_res = int * tlapm_obligation list * tlapm_notif list
 
 let empty = DocMap.empty
@@ -51,13 +62,13 @@ let empty = DocMap.empty
 (* Just record the text. It will be processed later, when a prover
    command or diagnostics query is issued by the client. *)
 let add docs uri vsn txt =
-  let rev = TV { text = txt; version = vsn } in
+  let rev = TV.make txt vsn in
   let drop_unused =
-    List.filter (fun (TV pv) -> pv.version < vsn - keep_vsn_count)
+    List.filter (fun (pv : TV.t) -> pv.version < vsn - keep_vsn_count)
   in
   let upd = function
-    | None -> Some (TD { pending = [ rev ]; actual = None; last_p_ref = 0 })
-    | Some (TD d) -> Some (TD { d with pending = rev :: drop_unused d.pending })
+    | None -> Some (TD.make rev)
+    | Some (d : TD.t) -> Some { d with pending = rev :: drop_unused d.pending }
   in
   DocMap.update uri upd docs
 
@@ -66,18 +77,16 @@ let rem docs uri = DocMap.remove uri docs
 let latest_vsn docs uri =
   match DocMap.find_opt uri docs with
   | None -> None
-  | Some (TD { actual; pending = []; _ }) -> (
+  | Some TD.{ actual; pending = []; _ } -> (
       match actual with
-      | Some (TA { doc_vsn; _ }) ->
-          let (TV doc_vsn) = doc_vsn in
-          Some doc_vsn.version
+      | Some { doc_vsn; _ } -> Some doc_vsn.version
       | None -> None)
-  | Some (TD { pending = TV dv :: _; _ }) -> Some dv.version
+  | Some TD.{ pending = dv :: _; _ } -> Some dv.version
 
-let with_actual (TD doc) uri docs f act pending =
-  let doc = TD { doc with pending; actual = Some act } in
-  let TD doc, act, res = f doc act in
-  let doc = TD { doc with actual = Some act } in
+let with_actual doc uri docs f act pending =
+  let doc = TD.{ doc with pending; actual = Some act } in
+  let doc, act, res = f doc act in
+  let doc = TD.{ doc with actual = Some act } in
   let docs = DocMap.add uri doc docs in
   (docs, res)
 
@@ -86,20 +95,10 @@ let with_actual (TD doc) uri docs f act pending =
 let with_doc_vsn docs uri vsn f =
   match DocMap.find_opt uri docs with
   | None -> (docs, None)
-  | Some (TD ({ pending; actual; _ } as doc)) -> (
-      let fresh_active (TV v) =
-        TA
-          {
-            doc_vsn = TV v;
-            p_ref = 0;
-            mule = None;
-            obs = OblMap.empty;
-            nts = [];
-          }
-      in
-      let merge_into (TV v) (TA a) =
-        let (TV a_doc_vsn) = a.doc_vsn in
-        let diff_pos = TlapmRange.first_diff_pos a_doc_vsn.text v.text in
+  | Some (TD.{ pending; actual; _ } as doc) -> (
+      let fresh_active v = TA.make v in
+      let merge_into (v : TV.t) (a : TA.t) =
+        let diff_pos = TlapmRange.first_diff_pos a.doc_vsn.text v.text in
         let before_change loc = not (TlapmRange.before diff_pos loc) in
         let obs =
           OblMap.filter
@@ -109,31 +108,31 @@ let with_doc_vsn docs uri vsn f =
         let nts =
           List.filter (fun (n : tlapm_notif) -> before_change n.loc) a.nts
         in
-        TA { a with doc_vsn = TV v; obs; nts }
+        { a with doc_vsn = v; obs; nts }
       in
       let rec drop_after_vsn = function
         | [] -> (None, [])
-        | TV pv :: pvs ->
-            if pv.version = vsn then (Some (TV pv), [])
+        | (pv : TV.t) :: pvs ->
+            if pv.version = vsn then (Some pv, [])
             else
               let res, pvs = drop_after_vsn pvs in
-              (res, TV pv :: pvs)
+              (res, pv :: pvs)
       in
       match actual with
-      | Some (TA ({ doc_vsn = TV adv; _ } as actual)) when adv.version = vsn ->
-          with_actual (TD doc) uri docs f (TA actual) pending
+      | Some ({ doc_vsn = adv; _ } as actual) when adv.version = vsn ->
+          with_actual doc uri docs f actual pending
       | _ -> (
           let selected, pending = drop_after_vsn pending in
           match selected with
           | None -> (docs, None)
           | Some selected ->
-              let (TA act') =
+              let act =
                 match actual with
                 | None -> fresh_active selected
                 | Some act -> merge_into selected act
               in
-              let act = TA { act' with mule = None } in
-              with_actual (TD doc) uri docs f act pending))
+              let act = { act with mule = None } in
+              with_actual doc uri docs f act pending))
 
 let analyze_mule (mule : Tlapm_lib.Module.T.mule) =
   let open Tlapm_lib in
@@ -161,9 +160,8 @@ let analyze_mule (mule : Tlapm_lib.Module.T.mule) =
   in
   ()
 
-let try_parse_anyway uri act =
-  let (TA a) = act in
-  let (TV v) = a.doc_vsn in
+let try_parse_anyway uri (act : TA.t) =
+  let v = act.doc_vsn in
   match
     (* TODO: Call the parser under a mutex. *)
     Tlapm_lib.module_of_string v.text (Lsp.Types.DocumentUri.to_path uri)
@@ -172,54 +170,53 @@ let try_parse_anyway uri act =
       Eio.traceln "-----------> Document parsed";
       analyze_mule mule;
       (* TODO: Fix it. *)
-      TA { a with mule = Some (Ok mule) }
+      { act with mule = Some (Ok mule) }
   | exception e ->
       (* TODO: Get the errors. *)
       Eio.traceln "-----------> Document parsing failed: %s"
         (Printexc.to_string e);
-      TA { a with mule = Some (Error e) }
+      { act with mule = Some (Error e) }
 
-let try_parse uri act =
+let try_parse uri (act : TA.t) =
   match act with
-  | TA { mule = None; _ } -> try_parse_anyway uri act
-  | TA { mule = Some _; _ } -> act
+  | { mule = None; _ } -> try_parse_anyway uri act
+  | { mule = Some _; _ } -> act
 
-let proof_res (TA a) =
-  let obs_list = List.map snd (OblMap.to_list a.obs) in
-  (a.p_ref, obs_list, a.nts)
+let proof_res (act : TA.t) =
+  let obs_list = List.map snd (OblMap.to_list act.obs) in
+  (act.p_ref, obs_list, act.nts)
 
 (* Push specific version to the actual, increase the proof_rec and clear the notifications. *)
 let prepare_proof docs uri vsn : t * (int * string * proof_res) option =
-  with_doc_vsn docs uri vsn @@ fun (TD doc') (TA act') ->
-  let (TA act') = try_parse uri (TA act') in
-  match act'.mule with
-  | None | Some (Error _) -> (TD doc', TA act', None)
+  with_doc_vsn docs uri vsn @@ fun (doc : TD.t) (act : TA.t) ->
+  let act = try_parse uri act in
+  match act.mule with
+  | None | Some (Error _) -> (doc, act, None)
   | Some (Ok _) ->
-      let next_p_ref = doc'.last_p_ref + 1 in
-      let doc = TD { doc' with last_p_ref = next_p_ref } in
-      let act = TA { act' with p_ref = next_p_ref; nts = [] } in
-      let (TV doc_vsn') = act'.doc_vsn in
-      (doc, act, Some (next_p_ref, doc_vsn'.text, proof_res act))
+      let next_p_ref = doc.last_p_ref + 1 in
+      let doc = { doc with last_p_ref = next_p_ref } in
+      let act = { act with p_ref = next_p_ref; nts = [] } in
+      (doc, act, Some (next_p_ref, act.doc_vsn.text, proof_res act))
 
 let add_obl docs uri vsn p_ref (obl : tlapm_obligation) =
-  with_doc_vsn docs uri vsn @@ fun (TD doc') (TA act') ->
-  if act'.p_ref = p_ref then
+  with_doc_vsn docs uri vsn @@ fun (doc : TD.t) (act : TA.t) ->
+  if act.p_ref = p_ref then
     let drop_older_intersecting (o_pr, _o_id) (o : tlapm_obligation) =
       o_pr = p_ref || not (TlapmRange.intersects obl.loc o.loc)
     in
-    let obs = OblMap.add (p_ref, obl.id) obl act'.obs in
+    let obs = OblMap.add (p_ref, obl.id) obl act.obs in
     let obs = OblMap.filter drop_older_intersecting obs in
-    let act = TA { act' with obs } in
-    (TD doc', act, Some (proof_res act))
-  else (TD doc', TA act', None)
+    let act = { act with obs } in
+    (doc, act, Some (proof_res act))
+  else (doc, act, None)
 
 let add_notif docs uri vsn p_ref notif =
-  with_doc_vsn docs uri vsn @@ fun (TD doc') (TA act') ->
-  if act'.p_ref = p_ref then
-    let nts = notif :: act'.nts in
-    let act = TA { act' with nts; obs = OblMap.empty } in
-    (TD doc', act, Some (proof_res act))
-  else (TD doc', TA act', None)
+  with_doc_vsn docs uri vsn @@ fun (doc : TD.t) (act : TA.t) ->
+  if act.p_ref = p_ref then
+    let nts = notif :: act.nts in
+    let act = { act with nts; obs = OblMap.empty } in
+    (doc, act, Some (proof_res act))
+  else (doc, act, None)
 
 let get_proof_res docs uri vsn =
   with_doc_vsn docs uri vsn @@ fun doc act ->
