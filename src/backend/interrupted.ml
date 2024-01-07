07e89f95c8db8303928d2e84e304a705cacb9e7a
Karolis Petrauskas <k.petrauskas@gmail.com>
2024-01-07 20:46:19 +0200

    Make mutex usage compatible with older ocaml versions.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- src/backend/interrupted.ml
+++ src/backend/interrupted.ml
@@ -1,9 +1,15 @@
 let state = ref false
 let lock = Mutex.create ()
-let is_interrupted () = Mutex.protect lock (fun () -> !state)
+
+let is_interrupted () =
+  Mutex.lock lock;
+  let state_val = !state in
+  Mutex.unlock lock;
+  state_val
 
 let mark_interrupted () =
-  Mutex.protect lock (fun () ->
-      let was = !state in
-      state := true;
-      was)
+  Mutex.lock lock;
+  let old_state = !state in
+  state := true;
+  Mutex.unlock lock;
+  old_state
