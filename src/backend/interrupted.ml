f653fd7931c8e092fe780ad29176728128c2b75b
Karolis Petrauskas <k.petrauskas@gmail.com>
2024-06-08 11:12:59 +0300

    Use atomic instead of mutex in the signal handler.
    Co-authored-by: Damien Doligez <damien.doligez@gmail.com>
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- src/backend/interrupted.ml
+++ src/backend/interrupted.ml
@@ -1,15 +1,3 @@
-let state = ref false
-let lock = Mutex.create ()
-
-let is_interrupted () =
-  Mutex.lock lock;
-  let state_val = !state in
-  Mutex.unlock lock;
-  state_val
-
-let mark_interrupted () =
-  Mutex.lock lock;
-  let old_state = !state in
-  state := true;
-  Mutex.unlock lock;
-  old_state
+let state = Atomic.make false
+let is_interrupted () = Atomic.get state
+let mark_interrupted () = Atomic.exchange state true
