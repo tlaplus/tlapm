52c8a4ad9d3b6a30a5360ed1f7240de2389f1ed7
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-10-16 16:05:28 -0400

    Syntax corpus files copied by dune build
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/parser/test_parser.ml
+++ test/parser/test_parser.ml
@@ -1,4 +1,19 @@
+open In_channel;;
+open List;;
+open Str;;
+
 let () =
     match Tlapm_lib.module_of_string "---- MODULE Test ---- x == 5 ====" with
     | None -> print_endline "Parse failed"
     | Some mule -> print_endline mule.core.name.core
+
+let () =
+    let test_file = open_in "syntax_corpus/modules.txt" in
+    try
+        let lines = input_lines test_file in
+        iter print_endline lines;
+        flush stdout;
+        close_in test_file
+    with e ->
+        close_in_noerr test_file;
+        raise e
