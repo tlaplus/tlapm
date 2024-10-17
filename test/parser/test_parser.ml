76b0b248305e0a2497641a8774e8f213aa37f820
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-10-17 18:18:11 -0400

    Basic parsing of test file format
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/parser/test_parser.ml
+++ test/parser/test_parser.ml
@@ -1,19 +1,43 @@
 open In_channel;;
 open List;;
 open Str;;
+open String;;
 
 let () =
     match Tlapm_lib.module_of_string "---- MODULE Test ---- x == 5 ====" with
     | None -> print_endline "Parse failed"
     | Some mule -> print_endline mule.core.name.core
+    
+let header_regex = regexp {|^===+||||}
 
-let () =
-    let test_file = open_in "syntax_corpus/modules.txt" in
+let separator_regex = regexp {|^---+||||}
+
+let parse_test_body input =
+    match split separator_regex input with
+    | input :: output :: [] -> (trim input, trim output)
+    | _ -> raise (Invalid_argument "Malformed test body")
+
+let rec parse_split_test_file input =
+    match input with
+    | [] -> []
+    | test_name :: test_body :: ls ->
+        let (test_input, test_output) = parse_test_body test_body in
+        (trim test_name, test_input, test_output) :: (parse_split_test_file ls)
+    | _ -> raise (Invalid_argument "Malformed test file")
+
+let parse_test_file path =
+    let test_file = open_in path in
     try
-        let lines = input_lines test_file in
-        iter print_endline lines;
-        flush stdout;
-        close_in test_file
+        let text = input_all test_file in
+        close_in test_file;
+        parse_split_test_file (split header_regex text)
     with e ->
         close_in_noerr test_file;
         raise e
+
+
+let () =
+    let tests = parse_test_file "syntax_corpus/modules.txt" in
+    let test_names = List.map (fun (a, _, _) -> a) tests in
+    List.iter print_endline test_names;
+    flush stdout;
