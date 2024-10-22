4476a3223b3b9acfc631c07cd06ee226ef0c73b6
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-10-22 19:57:28 -0400

    Added documentation to syntax corpus test file
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/parser/test_parser.ml
+++ test/parser/test_parser.ml
@@ -1,9 +1,22 @@
+(** This test runs a battery of TLA+ syntax fragments against TLAPM's syntax
+    parser. In the future it will check the actual parse tree emitted by
+    TLAPM, but for now it just checks whether TLAPM parses without error all
+    the syntax it is expected to parse. Tests sourced from:
+    https://github.com/tlaplus/tlaplus-standard/tree/main/tests/tlaplus_syntax
+*)
+
 open Syntax_corpus_file_parser;;
 
+(** Calls TLAPM's parser with the given input. Catches all exceptions and
+    treats them as parse failures.
+    @param input The TLA+ fragment to parse.
+    @return None if parse failure, syntax tree root if successful.
+*)
 let parse (input : string) : Tlapm_lib__M_t.mule option =
   try Tlapm_lib.module_of_string input
   with _ -> None
 
+(** Datatype summarizing a run of all the syntax tests. *)
 type test_run_summary = {
   total     : int;
   succeeded : int;
@@ -12,6 +25,7 @@ type test_run_summary = {
   failures  : syntax_test_info list;
 } [@@deriving show]
 
+(** A blank test summary. *)
 let test_summary_init = {
   total     = 0;
   succeeded = 0;
@@ -20,12 +34,14 @@ let test_summary_init = {
   failures  = [];
 }
 
+(** Summary for a successful test. *)
 let test_summary_succeeded = {
   test_summary_init with
   total = 1;
   succeeded = 1;
 }
 
+(** Summary for a failed test. *)
 let test_summary_failed info = {
   test_summary_init with
   total     = 1;
@@ -33,12 +49,14 @@ let test_summary_failed info = {
   failures  = [info];
 }
 
+(** Summary for a skipped test. *)
 let test_summary_skipped = {
   test_summary_init with
   total     = 1;
   skipped   = 1;
 }
 
+(** Merge and accumulate two test summaries into one. *)
 let acc_test_summary acc e = {
   total     = acc.total     + e.total;
   succeeded = acc.succeeded + e.succeeded;
@@ -47,6 +65,14 @@ let acc_test_summary acc e = {
   failures  = List.append acc.failures e.failures;
 }
 
+(** Runs a given syntax test. Checks whether test should be skipped, what
+    kind of test it is, and whether the test is expected to fail due to a
+    known bug in the TLAPM parser; then sends the test's TLA+ fragment into
+    the parser to see what happens.
+    @param should_fail Whether this test should fail due to a TLAPM bug.
+    @param test Information about the test itself.
+    @return A summary of the test's execution.
+*)
 let run_test (should_fail : syntax_test -> bool) (test : syntax_test) : test_run_summary =
   if test.skip then test_summary_skipped else
   match test.test with
@@ -59,6 +85,14 @@ let run_test (should_fail : syntax_test -> bool) (test : syntax_test) : test_run
     | None -> if should_fail test then test_summary_succeeded else test_summary_failed test.info
     | Some _ -> if should_fail test then test_summary_failed test.info else test_summary_succeeded)
 
+(** Given a path to a directory containing a corpus of syntax tests, get all
+    the tests encoded in those files, filter them as appropriate, then run
+    them all and collect the results.
+    @param path Path to the directory containing the corpus of syntax tests.
+    @param should_fail Whether a test should fail due to a TLAPM bug.
+    @param filter_predicate Whether to actually execute a test.
+    @return Accumulated summary of all test executions.
+*)
 let run_test_corpus (path : string) (should_fail : syntax_test -> bool) (filter_pred : syntax_test -> bool)  : test_run_summary =
   path
   |> get_all_tests_under
@@ -66,7 +100,8 @@ let run_test_corpus (path : string) (should_fail : syntax_test -> bool) (filter_
   |> List.map (run_test should_fail)
   |> List.fold_left acc_test_summary test_summary_init
 
-let error_test_names = [
+(** Names of tests that are known to fail due to TLAPM parser bugs. *)
+let failing_test_names = [
   "Invalid parentheses use in jlist";
   "Bounded Quantification With Tuples";
   "Mixed Bounded Quantification With Tuples";
@@ -91,13 +126,14 @@ let error_test_names = [
   "Proof Step ID Subexpression Tree Navigation";
 ]
 
+(** The top-level test; runs all syntax tests, prints summary, then fails
+    with an assertion if any tests failed.
+*)
 let () =
   (* Keeping this around because it's useful when wanting to run a single test. *)
   (*let filter_pred = fun test -> String.equal test.info.name "Proof Containing Jlist" in*)
   let filter_pred = fun _ -> true in
-  let test_results = run_test_corpus
-    "syntax_corpus"
-    (fun test -> List.mem test.info.name error_test_names)
-    filter_pred
-  in print_endline (show_test_run_summary test_results);
+  let should_fail = fun (test : syntax_test) -> List.mem test.info.name failing_test_names in
+  let test_results = run_test_corpus "syntax_corpus" should_fail filter_pred in
+  print_endline (show_test_run_summary test_results);
   ignore (assert (0 == test_results.failed));
