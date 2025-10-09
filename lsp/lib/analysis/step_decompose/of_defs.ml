df19b69be67537180be3368ff57a4f7e96fd25a2
Karolis Petrauskas <k.petrauskas@gmail.com>
2025-10-09 07:51:44 +0300

    Cleanup.
    Signed-off-by: Karolis Petrauskas <k.petrauskas@gmail.com>

--- lsp/lib/analysis/step_decompose/of_defs.ml
+++ lsp/lib/analysis/step_decompose/of_defs.ml
@@ -2,64 +2,60 @@ open Util
 
 (** List expandable names in the expression and its context. *)
 let expandable_names (cx : TL.Expr.T.ctx) (ex : TL.Expr.T.expr) : string list =
+  let open TL.Expr in
   let names = ref StringSet.empty in
   let add_name n = names := StringSet.add n !names in
   let visitor =
     object (_self : 'self)
-      inherit [unit] TL.Expr.Visit.iter as super
+      inherit [unit] Visit.iter as super
 
-      method! expr (cx : unit TL.Expr.Visit.scx) (e : TL.Expr.T.expr) =
+      method! expr (cx : unit Visit.scx) (e : T.expr) =
         (match e.core with
-        | TL.Expr.T.Opaque name -> add_name name
-        | TL.Expr.T.Ix ix -> (
-            let hyp = TL.Expr.T.get_val_from_id (snd cx) ix in
-            let cx = TL.Expr.T.cx_front (snd cx) ix in
+        | T.Opaque name -> add_name name
+        | T.Ix ix -> (
+            let hyp = T.get_val_from_id (snd cx) ix in
+            let cx = T.cx_front (snd cx) ix in
             match hyp |> unwrap with
-            | TL.Expr.T.Fresh (_, _, _, _)
-            | TL.Expr.T.FreshTuply (_, _)
-            | TL.Expr.T.Flex _ ->
-                ()
-            | TL.Expr.T.Defn (defn, _, TL.Expr.T.Visible, _) ->
+            | T.Fresh (_, _, _, _) | T.FreshTuply (_, _) | T.Flex _ -> ()
+            | T.Defn (defn, _, T.Visible, _) ->
                 ignore (super#defn ((), cx) defn)
-            | TL.Expr.T.Defn (_, _, TL.Expr.T.Hidden, _) ->
-                add_name (TL.Expr.T.hyp_name hyp)
-            | TL.Expr.T.Fact (ex, TL.Expr.T.Visible, _) ->
-                super#expr ((), cx) ex
-            | TL.Expr.T.Fact (_, TL.Expr.T.Hidden, _) -> ())
-        | TL.Expr.T.Internal _
-        | TL.Expr.T.Lambda (_, _)
-        | TL.Expr.T.Sequent _
-        | TL.Expr.T.Bang (_, _)
-        | TL.Expr.T.Apply (_, _)
-        | TL.Expr.T.With (_, _)
-        | TL.Expr.T.If (_, _, _)
-        | TL.Expr.T.List (_, _)
-        | TL.Expr.T.Let (_, _)
-        | TL.Expr.T.Quant (_, _, _)
-        | TL.Expr.T.QuantTuply (_, _, _)
-        | TL.Expr.T.Tquant (_, _, _)
-        | TL.Expr.T.Choose (_, _, _)
-        | TL.Expr.T.ChooseTuply (_, _, _)
-        | TL.Expr.T.SetSt (_, _, _)
-        | TL.Expr.T.SetStTuply (_, _, _)
-        | TL.Expr.T.SetOf (_, _)
-        | TL.Expr.T.SetOfTuply (_, _)
-        | TL.Expr.T.SetEnum _ | TL.Expr.T.Product _ | TL.Expr.T.Tuple _
-        | TL.Expr.T.Fcn (_, _)
-        | TL.Expr.T.FcnTuply (_, _)
-        | TL.Expr.T.FcnApp (_, _)
-        | TL.Expr.T.Arrow (_, _)
-        | TL.Expr.T.Rect _ | TL.Expr.T.Record _
-        | TL.Expr.T.Except (_, _)
-        | TL.Expr.T.Dot (_, _)
-        | TL.Expr.T.Sub (_, _, _)
-        | TL.Expr.T.Tsub (_, _, _)
-        | TL.Expr.T.Fair (_, _, _)
-        | TL.Expr.T.Case (_, _)
-        | TL.Expr.T.String _
-        | TL.Expr.T.Num (_, _)
-        | TL.Expr.T.At _
-        | TL.Expr.T.Parens (_, _) ->
+            | T.Defn (_, _, T.Hidden, _) -> add_name (T.hyp_name hyp)
+            | T.Fact (ex, T.Visible, _) -> super#expr ((), cx) ex
+            | T.Fact (_, T.Hidden, _) -> ())
+        | T.Internal _
+        | T.Lambda (_, _)
+        | T.Sequent _
+        | T.Bang (_, _)
+        | T.Apply (_, _)
+        | T.With (_, _)
+        | T.If (_, _, _)
+        | T.List (_, _)
+        | T.Let (_, _)
+        | T.Quant (_, _, _)
+        | T.QuantTuply (_, _, _)
+        | T.Tquant (_, _, _)
+        | T.Choose (_, _, _)
+        | T.ChooseTuply (_, _, _)
+        | T.SetSt (_, _, _)
+        | T.SetStTuply (_, _, _)
+        | T.SetOf (_, _)
+        | T.SetOfTuply (_, _)
+        | T.SetEnum _ | T.Product _ | T.Tuple _
+        | T.Fcn (_, _)
+        | T.FcnTuply (_, _)
+        | T.FcnApp (_, _)
+        | T.Arrow (_, _)
+        | T.Rect _ | T.Record _
+        | T.Except (_, _)
+        | T.Dot (_, _)
+        | T.Sub (_, _, _)
+        | T.Tsub (_, _, _)
+        | T.Fair (_, _, _)
+        | T.Case (_, _)
+        | T.String _
+        | T.Num (_, _)
+        | T.At _
+        | T.Parens (_, _) ->
             ());
         super#expr cx e
     end
@@ -69,12 +65,12 @@ let expandable_names (cx : TL.Expr.T.ctx) (ex : TL.Expr.T.expr) : string list =
     | None -> ()
     | Some (cx, hyp) ->
         (match hyp |> unwrap with
-        | TL.Expr.T.Fresh (_, _, _, _)
-        | TL.Expr.T.FreshTuply (_, _)
-        | TL.Expr.T.Flex _
-        | TL.Expr.T.Defn (_, _, _, _) ->
+        | T.Fresh (_, _, _, _)
+        | T.FreshTuply (_, _)
+        | T.Flex _
+        | T.Defn (_, _, _, _) ->
             ()
-        | TL.Expr.T.Fact (_, _, _) -> ignore (visitor#hyp ((), cx) hyp));
+        | T.Fact (_, _, _) -> ignore (visitor#hyp ((), cx) hyp));
         traverse_cx cx
   in
   visitor#expr ((), cx) ex;
@@ -82,7 +78,9 @@ let expandable_names (cx : TL.Expr.T.ctx) (ex : TL.Expr.T.expr) : string list =
   StringSet.to_list !names |> List.sort String.compare
 
 (** Propose expand actions for all the definitions that are visible, but not yet
-    expanded. *)
+    expanded.
+
+    TODO: "Expand all definitions". *)
 let cas_def_expand ~uri ~(ps : PS.t) ~cx ~by ~(sq : TL.Expr.T.sequent) =
   expandable_names sq.context sq.active
   |> List.map @@ fun def_name ->
