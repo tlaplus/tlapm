2973c8fcb8601d0d498c24c4dff41ec1412764bf
Andrew Helwer <ahelwer@pm.me>
2025-12-07 18:00:22 -0800

    Stubbed out SANY parse architecture
    Signed-off-by: Andrew Helwer <ahelwer@pm.me>

--- src/sany.ml
+++ src/sany.ml
@@ -1,1 +1,24 @@
-let parse module_path = Result.Error (None, "TODO")
+let source_to_sany_xml (module_path : string) : (string, int) result =
+  Error 1
+
+type parsed_xml = unit
+let parse_xml (xml_ast : string) : parsed_xml = ()
+
+let convert_ast (ast : parsed_xml) : Module.T.modctx * Module.T.mule =
+  let open Util.Coll in
+  let open Property in
+  let open Module.T in
+  (Sm.empty, noprops {
+    name = noprops "Placeholder";
+    extendees = [];
+    instancees = [];
+    body = [];
+    defdepth = 0;
+    stage = Parsed;
+    important = true
+  })
+
+let parse module_path =
+  match module_path |> source_to_sany_xml with
+  | Error exit_code -> Error (None, Int.to_string exit_code)
+  | Ok xml_ast -> xml_ast |> parse_xml |> convert_ast |> Result.ok
